6d8789c5a2362df95a50818bda6e320ef082bf68
Meqdad <meqdad.moradi@outlook.com>
2023-04-25 20:23:28 +0200

    auto refresh added

--- src/app/components/dialogs/result/result.component.ts
+++ src/app/components/dialogs/result/result.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 import { ITotalTimes } from 'src/app/modules/times';
 
 @Component({
@@ -8,25 +9,12 @@ import { ITotalTimes } from 'src/app/modules/times';
   styleUrls: ['./result.component.scss'],
 })
 export class ResultComponent implements OnInit {
-  totalTime: any;
-
-  /**
-   * constructor
-   * @param matDialogRef matDialogRef
-   * @param data mat dialog data (hour & minutes)
-   */
   constructor(
     private matDialogRef: MatDialogRef<ResultComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: ITotalTimes
+    @Inject(MAT_DIALOG_DATA) public data: Observable<ITotalTimes>
   ) {}
 
-  ngOnInit(): void {
-    console.log(this.data);
-    this.totalTime = {
-      hour: String(this.data.hour).padStart(2, '0'),
-      mins: String(this.data.mins).padStart(2, '0'),
-    };
-  }
+  ngOnInit(): void {}
 
   closeDialog(): void {
     this.matDialogRef.close();
